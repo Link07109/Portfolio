4c0c26f3b0b6a6f12f5d8216a9b797bc1c435c5c
Ivan Valadez <svaladez@uncc.edu>
2022-05-17 15:40:36 -0400

    more readable code

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -11,10 +11,18 @@ const NavBar = () => {
 
     return (
         <header className='header'>
-            <button className='nav-button' onClick={() => navigate('/')}><h2 className='link fancy-link'>Home</h2></button>
-            <button className='nav-button' onClick={() => navigate('/projects')}><h2 className='link fancy-link'>Projects</h2></button>
-            <button className='nav-button' onClick={() => navigate('/about')}><h2 className='link fancy-link'>About</h2></button>
-            <button className='nav-button' onClick={() => navigate('/contact')}><h2 className='link fancy-link'>Contact</h2></button>
+            <button className='nav-button' onClick={() => navigate('/')}>
+                <h2 className='link fancy-link'>Home</h2>
+            </button>
+            <button className='nav-button' onClick={() => navigate('/projects')}>
+                <h2 className='link fancy-link'>Projects</h2>
+            </button>
+            <button className='nav-button' onClick={() => navigate('/about')}>
+                <h2 className='link fancy-link'>About</h2>
+            </button>
+            <button className='nav-button' onClick={() => navigate('/contact')}>
+                <h2 className='link fancy-link'>Contact</h2>
+            </button>
             
             <ThemeContext.Consumer className='center'>
                 {({ changeTheme }) => (
